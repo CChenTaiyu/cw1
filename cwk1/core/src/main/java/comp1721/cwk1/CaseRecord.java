81067e0be18bfa3489c590d3b439aa422024c676
泰宇 陈 <1271324025@qq.com>
2021-03-27 16:38:45 +0800

    Initial files for Coursework 1

---
+++ cwk1/core/src/main/java/comp1721/cwk1/CaseRecord.java
@@ -0,0 +1,21 @@
+package comp1721.cwk1;
+
+public class CaseRecord {
+  // TODO: Write stub for constructor
+	public caseRecord(LocalDate date, int staffCases, int studentCases, int otherCases)
+  // TODO: Write stubs for four getter methods
+	LocalDate date(){
+	}
+	int getStaffCases{
+	}
+	int getStudentCases{
+	}
+	int otherCases{
+	}
+  // TODO: Write stub for totalCases()
+	int totalCases{
+	}
+  // TODO: Write stub for toString()
+	String toString{
+	}
+ 
